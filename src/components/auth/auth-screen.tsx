279d22dc7098079837340a4a858a962e9f6e9938
romanovmaksim <massib796@gmail.com>
2024-09-13 09:54:05 +0300

    small bugfix

--- src/components/auth/auth-screen.tsx
+++ src/components/auth/auth-screen.tsx
@@ -4,7 +4,7 @@ import { SignInCard } from "./sign-in-card";
 
 export function AuthScreen() {
   return (
-    <div className="h-full flex items-center justify-center bg-[#5C3B58]">
+    <div className="h-full flex items-center justify-center bg-gray-700">
       <div className="md:h-auto md:w-[420px]">
         <SignInCard></SignInCard>
       </div>
